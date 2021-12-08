996869c08945dffccf9fb018ed4b95c89e31ec6c
Sophie Spang <soph8111@stud.kea.dk>
2021-12-08 10:40:20 +0100

    article read more template

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -29,24 +29,37 @@ export default function Beer(props) {
     // props.setBasket(props.basket.filter((x) => x.name == props.name));
   }
 
-  function readMore() {}
+  function readMore() {
+    console.log("heeej");
+  }
 
   return (
-    <article className="Beer">
-      <h2>{props.name}</h2>
-      <img src={`../images/${props.label}`} alt={props.name} />
-      <p className="Alc">{props.alc}% alc</p>
-      <p className="Type">{props.category}</p>
-      <p className="Impression">{props.description.overallImpression}</p>
-      <h3 className="Price">50 DKK</h3>
-      {/* <button onClick={readMore} className="Read_more">
-        Read more
-      </button> */}
-      <span className="addOrRemove">
-        <button onClick={removeOne}>-</button>
-        <p>{amount}</p>
-        <button onClick={addOne}>+</button>
-      </span>
-    </article>
+    <>
+      <article className="Beer">
+        <h2>{props.name}</h2>
+        <img src={`../images/${props.label}`} alt={props.name} />
+        <p className="Alc">{props.alc}% alc</p>
+        <p className="Type">{props.category}</p>
+        <h3 className="Price">50 DKK</h3>
+        <button onClick={readMore} className="Read_more">
+          Read more
+        </button>
+        <span className="addOrRemove">
+          <button onClick={removeOne}>-</button>
+          <p>{amount}</p>
+          <button onClick={addOne}>+</button>
+        </span>
+      </article>
+
+      <article class="about_beer">
+        <h2 class="name">{props.name}</h2>
+        <h3 class="category"></h3>
+        <p class="alc"></p>
+        <p class="aroma"></p>
+        <p class="appearance"></p>
+        <p class="flavor"></p>
+        <p class="mouthfeel"></p>
+      </article>
+    </>
   );
 }
