9e25760e11c95182db20fd8264ca9bb95f3ae16c
Sophie Spang <soph8111@stud.kea.dk>
2021-12-08 13:59:51 +0100

    checkmarks and basic styling on form input

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,23 +11,33 @@ export default function Form(props) {
       <form action="">
         <div className="Form-group">
           <label htmlFor="name">Your name</label>
-          <p>Enter your name</p>
-          <input type="text" name="name" id="" />
+          <span className="Checkmark">
+            <input type="text" id="name" name="name" placeholder="E.g. John Dillermand" required />
+          </span>
+          <span className="Error">Please enter your full name</span>
         </div>
         <div className="Form-group">
           <label htmlFor="cardno">Card Number</label>
-          <p>Enter your credit card number</p>
-          <input type="number" name="cardno" id="" placeholder="xxxx xxxx xxxx xxxx" />
+          <span className="Checkmark">
+            <input type="tel" id="cardno" name="cardno" inputmode="numeric" placeholder="XXXX XXXX XXXX XXXX" pattern="[0-9]{16}" required />
+          </span>
+          <span className="Error">Must be 16 characters long</span>
         </div>
         <div className="Form-group">
-          <label htmlFor="expire">Expiration Date</label>
-          <p>e.g. 06 / 12 </p>
-          <input type="number" name="expire" id="" placeholder="MM / YYYY" />
+          <label htmlFor="expire-month">Expiry Date</label>
+          <span className="Checkmark">
+            <input type="tel" id="expire-month" name="expire-month" inputmode="numeric" placeholder="MM" required pattern="[0-9]{2}" />
+            <label htmlFor="expire-year"></label>
+            <input type="tel" id="expire-year" name="expire-year" inputmode="numeric" placeholder="YYYY" required pattern="[0-9]{4}" />
+          </span>
+          <span className="Error">Eg. 01/2022</span>
         </div>
         <div className="Form-group">
-          <label htmlFor="cvc">Expiration Date</label>
-          <p>e.g. 123</p>
-          <input type="number" name="cvc" id="" placeholder="CVC" />
+          <label htmlFor="cvc">CVC</label>
+          <span className="Checkmark">
+            <input type="tel" id="cvc" name="cvc" inputmode="numeric" placeholder="XXX" required pattern="[0-9]{3}"></input>
+          </span>
+          <span className="Error">Security code (3 digits on back of card)</span>
         </div>
         <p className="sumTotal">{sum} DKK</p>
         <button id="pay" type="submit">
