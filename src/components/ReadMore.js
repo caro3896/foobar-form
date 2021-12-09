32ecc9a08acf0ec378731030e2d03c5c604dc8ea
Helena <hele362c@stud.kea.dk>
2021-12-09 09:39:40 +0100

    modal

--- src/components/ReadMore.js
+++ src/components/ReadMore.js
@@ -2,13 +2,13 @@ export default function ReadMore() {
   return (
     <template class="about">
       <article class="about_beer">
-        <h2 class="name"></h2>
-        <h3 class="category"></h3>
-        <p class="alc"></p>
-        <p class="aroma"></p>
-        <p class="appearance"></p>
-        <p class="flavor"></p>
-        <p class="mouthfeel"></p>
+        <h2 className="NameModal"></h2>
+        <h3 className="CategoryModal"></h3>
+        <p className="AlcModal"></p>
+        <p className="AromaModal"></p>
+        <p className="AppearanceModal"></p>
+        <p className="FlavorModal"></p>
+        <p className="MouthfeelModal"></p>
       </article>
     </template>
   );
